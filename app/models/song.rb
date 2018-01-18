f7bba611d8f9b2305709f2755094f10825fb67d0
Olena Ageyeva <eageeva@hotmail.com>
2018-01-18 01:42:29 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,11 @@ class Song < ActiveRecord::Base
     self.artist.name
   end
 
+  def artist_name=(name)
+   artist = Artist.find_or_create_by(name: name)
+   self.artist = artist
+  end
+
   def genre_name
     self.genre.name
   end
