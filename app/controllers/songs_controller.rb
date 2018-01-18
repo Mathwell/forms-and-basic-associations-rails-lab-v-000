87e6e9dc95bffdd59c9eeae9754d15d69ac8d72d
Olena Ageyeva <eageeva@hotmail.com>
2018-01-18 02:22:54 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,6 +42,14 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    flash[:notice] = "Song deleted."
+    redirect_to songs_path
+  end
+
+
   private
 
   def song_params
