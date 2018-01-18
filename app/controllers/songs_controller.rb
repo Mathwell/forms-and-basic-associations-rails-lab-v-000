60f259810b18d687be62bd982c1b67cec06e4c15
Olena Ageyeva <eageeva@hotmail.com>
2018-01-18 02:20:54 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,8 +17,13 @@ class SongsController < ApplicationController
 
   def create
     p song_params
-    song = Song.create(song_params)
-    redirect_to controller: 'songs', action: 'index'
+    song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+    #redirect_to controller: 'songs', action: 'index'
   end
 
   private
